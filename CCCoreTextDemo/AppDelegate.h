e6e3cc7fe7446f98456e182c0d2d1aa011f8fadb
CC (deng you hua) <ccworld1000@gmail.com>
2020-12-29 19:13:34 +0800

    CCAutoTag push : update

---
+++ CCCoreTextDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  CCCoreTextDemo
+//
+//  Created by CC on 2020/12/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ CCCoreTextDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  CCCoreTextDemo
+//
+//  Created by CC on 2020/12/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
